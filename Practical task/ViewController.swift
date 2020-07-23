4a713bf4e3658741005e44a15c5bed10892a1dd1
Yoel Jimenez del Valle <yoel07@gmail.com>
2020-07-23 17:59:14 -0400

    Initial Commit

---
+++ Practical task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practical task
+//
+//  Created by Yoel Jimenez del Valle on 7/23/20.
+//  Copyright © 2020 Yoel Jimenez del Valle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
